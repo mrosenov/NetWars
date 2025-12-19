8f00d370d3bf67cf8d2b1e696c49e709d6e828da
Mitko Rosenov <mitkorosenov@live.com>
2025-12-19 02:34:48 +0200

    breeze added

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,15 @@
+<div {{$attributes}}>
+    <div class="group flex items-center gap-3 bg-[#0b0f14] px-6 py-3 rounded-lg select-none">
+        <!-- Terminal Icon -->
+        <span class="font-mono text-xl font-bold text-green-400 glow leading-none">
+            &gt;<span class="blink">_</span>
+        </span>
+
+        <!-- Logo Text -->
+        <span class="relative font-semibold text-lg tracking-wide">
+            <span class="text-white">Net</span>
+            <span class="text-green-400 glow group-hover:glitch">Wars</span>
+        </span>
+    </div>
+</div>
+
